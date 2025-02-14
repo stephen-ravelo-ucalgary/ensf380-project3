ffe53fc56dc54bbb872436e91171ded644057c83
boopitis <stephenravelo31@gmail.com>
2025-02-14 13:33:22 -0700

    Create classes

---
+++ src/edu/ucalgary/oop/Location.java
@@ -0,0 +1,8 @@
+package edu.ucalgary.oop;
+
+public class Location {
+    private String name;
+    private String address;
+    private DisasterVictim[] occupants;
+    private Supply[] supplies;
+}
