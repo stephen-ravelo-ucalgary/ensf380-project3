12f6aac6f10f9e6bdde645d3554ca3ee909b1286
kingjulian3000 <adrina.sadeghian@ucalgary.ca>
2025-02-26 22:22:35 -0700

    Update Inquirer.java

--- src/edu/ucalgary/oop/Inquirer.java
+++ src/edu/ucalgary/oop/Inquirer.java
@@ -1,5 +1,32 @@
 package edu.ucalgary.oop;
 
 public class Inquirer {
+    private String FIRST_NAME;
+    private String LAST_NAME;
+    private String INFO;
+    private String SERVICES_PHONE;
+
+    public Inquirer(String firstName,String lastName, String phone, String info){
+        this.FIRST_NAME = firstName;
+        this.LAST_NAME = lastName;
+        this.INFO = info;
+        this.SERVICES_PHONE = phone;
+    } 
+
+    public String getFirstName(){
+        return FIRST_NAME;
+    }
+
+    public String getLastName(){
+        return LAST_NAME;
+    }
+    
+    public String getServicesPhoneNum(){
+        return SERVICES_PHONE;
+    }
+    
+    public String getInfo(){
+        return INFO;
+    }
 
 }
